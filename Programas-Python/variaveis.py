e64681789cf1a417900ff3e1dff5fffd9c285120
Gabriel Leão da Silva <gabriellsilva1200@outlook.com.br>
2021-12-28 17:42:05 -0300

    Data: 28/12/21 às 17:42
    Foram criadas as pastas Programas-Jupyter onde ficará todos os arquivos .ipynb, e também foi criada a pasta Programas-Python onde ficará todos os arquivos .py
    Além disso os arquivos hello.py e hello.ipynb foram movidos para as suas respectivas pastas, e foi criado os arquivos variaveis.ipynb e variaveis.py que também foram movidos para suas pastas.

---
+++ Programas-Python/variaveis.py
@@ -0,0 +1,10 @@
+x = 5
+preco = 9.99
+nome = 'Gabriel'
+verdadeiro = True
+falso = False
+print(x, type(x))
+print(preco, type(preco))
+print(nome, type(nome))
+print(verdadeiro, type(verdadeiro))
+print(falso, type(falso))
