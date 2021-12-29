95038992f17b67344d186a9a5f8925287d3bd7a7
Gabriel Leão da Silva <gabriellsilva1200@outlook.com.br>
2021-12-29 14:02:59 -0300

    Data 29/12/21 às 14:02
    Erro arrumado.

--- Programas-Python/estrutura-while.py
+++ Programas-Python/estrutura-while.py
@@ -1,14 +1,19 @@
 n = 0
 s = 0 
 nd = 0
+resp = 's'
 
-while True:
+while resp == 's' or resp == 'S':
     n = int(input('Digite um número: '))
     s += n
     nd += 1
-    resp = str(input('Deseja continuar? [S/N]'))
-    if resp == 'S' or resp == 's':
-        continue
-    elif resp == 'N' or resp =='n':
-        break
+    while resp != 's' or resp != 'S' or resp != 'n' or resp != 'N':
+        resp = str(input('Deseja continuar? [S/N]'))
+        if resp == 'S' or resp == 's':
+            break
+        elif resp == 'N' or resp =='n':
+            break
+        else:
+            print('Opção inválida. Tente novamente')
+            continue
 print(f'Ao todo foram digitados {nd} números e a soma entre eles é {s}')
